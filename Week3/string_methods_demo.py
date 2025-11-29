117d81c363d06c97385f2948dffc2c93c49d6db7
gudduleuva-hub <gudduleuva@gmail.com>
2025-11-29 10:54:38 +0530

    Create string_methods_demo.py

---
+++ Week3/string_methods_demo.py
@@ -0,0 +1,8 @@
+# String Methods Demo
+
+text = "Hello Python Programming"
+
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Split:", text.split())
+print("Replace:", text.replace("Python", "Coding"))
